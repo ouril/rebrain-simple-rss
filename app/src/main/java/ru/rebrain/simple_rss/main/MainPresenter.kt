d745f176bc8c0610e386a7d05772b0efbc38d78c
Eugene Saturov <saturov@surfstudio.ru>
2018-12-06 22:41:11 +0300

    Реализация MainPresenter

---
+++ app/src/main/java/ru/rebrain/simple_rss/main/MainPresenter.kt
@@ -0,0 +1,15 @@
+package ru.rebrain.simple_rss.main
+
+import ru.rebrain.simple_rss.base.presenter.BasePresenter
+import java.util.*
+
+class MainPresenter : BasePresenter<MainContract.View>(), MainContract.Presenter {
+
+    private val helloTexts = listOf("BONJOUR", "HOLA", "HALLO", "MERHABA", "HELLO", "CIAO", "KONNICHIWA")
+
+    override fun loadHelloText() {
+        val random = Random()
+        val hello = helloTexts[random.nextInt(helloTexts.size)]
+        view?.onTextLoaded(hello)
+    }
+}
