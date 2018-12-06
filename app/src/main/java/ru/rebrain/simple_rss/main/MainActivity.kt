83c2859d634b6384bbc2ccc405683be73c59450b
Eugene Saturov <saturov@surfstudio.ru>
2018-12-06 22:42:54 +0300

    Реализация MainActivity

--- app/src/main/java/ru/rebrain/simple_rss/main/MainActivity.kt
+++ app/src/main/java/ru/rebrain/simple_rss/main/MainActivity.kt
@@ -1,19 +1,32 @@
 package ru.rebrain.simple_rss.main
 
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.rebrain.simple_rss.R
 import ru.rebrain.simple_rss.base.view.BaseActivity
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), MainContract.View {
+
+    private lateinit var presenter: MainContract.Presenter
 
     override val layoutResId = R.layout.activity_main
 
     override fun init(state: Bundle?) {
-        //todo
+        presenter = MainPresenter()
+        presenter.attach(this)
+        presenter.loadHelloText()
+
+        hello_tv.setOnClickListener {
+            presenter.loadHelloText()
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        //todo
+        presenter.detach()
+    }
+
+    override fun onTextLoaded(text: String) {
+        hello_tv.text = text
     }
 }
